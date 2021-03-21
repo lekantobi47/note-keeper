f103e6a7dca3636d61e059b2f8b1d612852205ca
Olamilekan Akintilebo <lekantobi47@gmail.com>
2021-03-21 13:43:47 +0100

    updated RecyclerViewAdapter.kt and RecyclerViewFragment.kt

--- app/src/main/java/com/hayohtee/notekeeper/RecyclerViewAdapter.kt
+++ app/src/main/java/com/hayohtee/notekeeper/RecyclerViewAdapter.kt
@@ -1,25 +1,34 @@
 package com.hayohtee.notekeeper
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewAdapter(): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
-
+        val position = view.findViewById(R.id.text_position) as TextView
+        val content = view.findViewById(R.id.text_content) as TextView
     }
 
+    // dummy list
     private val names = arrayOf("James", "John", "Caleb", "Sarah")
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item_view,
+        parent, false)
+
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.position.text = (position + 1).toString()
+        holder.content.text = names[position]
     }
 
     override fun getItemCount() = names.size
-
 
 }
--- app/src/main/java/com/hayohtee/notekeeper/fragments/RecyclerViewFragment.kt
+++ app/src/main/java/com/hayohtee/notekeeper/fragments/RecyclerViewFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.hayohtee.notekeeper.R
+import com.hayohtee.notekeeper.RecyclerViewAdapter
 import com.hayohtee.notekeeper.databinding.FragmentRecyclerViewBinding
 
 class RecyclerViewFragment : Fragment() {
@@ -16,7 +19,16 @@ class RecyclerViewFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recycler_view,
             container, false)
 
+        setupRecyclerView()
+
         return binding.root
     }
 
+
+    private fun setupRecyclerView(){
+        StaggeredGridLayoutManager(2, RecyclerView.VERTICAL).apply {
+            binding.recyclerView.layoutManager = this
+        }
+        binding.recyclerView.adapter = RecyclerViewAdapter()
+    }
 }
