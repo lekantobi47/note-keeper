55c028992c95fde3c363987f5db8a8a817949c81
Olamilekan Akintilebo <lekantobi47@gmail.com>
2021-03-21 14:01:02 +0100

    created Home frament and associated resource file, closes #2.

---
+++ app/src/main/java/com/hayohtee/notekeeper/fragments/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.hayohtee.notekeeper.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.hayohtee.notekeeper.R
+import com.hayohtee.notekeeper.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home,
+            container,false)
+
+        return binding.root
+    }
+}
