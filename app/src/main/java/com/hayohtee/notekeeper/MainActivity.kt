395ef5ae49541f08fcd72a22c6468b15f9c5267b
Olamilekan Akintilebo <lekantobi47@gmail.com>
2021-03-21 15:30:55 +0100

    added support for up navigation in MainActivity.kt, closes #12

--- app/src/main/java/com/hayohtee/notekeeper/MainActivity.kt
+++ app/src/main/java/com/hayohtee/notekeeper/MainActivity.kt
@@ -3,14 +3,28 @@ package com.hayohtee.notekeeper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.hayohtee.notekeeper.model.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
     val noteViewModel: NoteViewModel by lazy {
         ViewModelProvider(this).get(NoteViewModel::class.java)
     }
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = this.findNavController(R.id.nav_host_fragment_container)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
 }
